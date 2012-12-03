6a9ae0efcc8f3807d8a8f0ce5f0502a068e6110a
timothy dai <timothy.dai@hotmail.com>
2012-12-03 01:16:02 -0700

    remove alert

--- www/js/index.js
+++ www/js/index.js
@@ -27,12 +27,13 @@ var app = {
     deviceready: function() {
         // This is an event handler function, which means the scope is the event.
         // So, we must explicitly called `app.report()` instead of `this.report()`.
+		confirm(" envent....");
         app.report('deviceready');
     },
     report: function(id) {
         // Report the event in the console
         console.log("Report: " + id);
-		alert("Report: " + id);
+		
 
         // Toggle the state from "pending" to "complete" for the reported ID.
         // Accomplished by adding .hide to the pending element and removing
